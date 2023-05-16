cfe5d7b738c64be6a5180ec92c344670ecf446eb
Raman Samusevich <raman.samusevich@honeywell.com>
2023-05-16 11:10:54 -0400

    coms generation, more samples

--- design_baselines/coms_cleaned/experiments.py
+++ design_baselines/coms_cleaned/experiments.py
@@ -795,7 +795,7 @@ def aav(local_dir, cpus, gpus, num_parallel, num_samples):
             "forward_model_batch_size": 128,
             "forward_model_val_size": 500,
             "forward_model_epochs": 50,
-            "evaluation_samples": 1900,
+            "evaluation_samples": 2500,
             "fast": False
         },
              num_samples=num_samples,
