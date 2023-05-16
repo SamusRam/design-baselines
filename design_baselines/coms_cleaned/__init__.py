dfae10029f1becd679bb75534d693f11a848c8a1
Raman Samusevich <raman.samusevich@honeywell.com>
2023-05-15 23:17:45 -0400

    mapping coms predictions to integers

--- design_baselines/coms_cleaned/__init__.py
+++ design_baselines/coms_cleaned/__init__.py
@@ -340,7 +340,7 @@ config):
                 solution = solution * standard_dev + mean
                 logits = vae_model.decoder_cnn.predict(solution)
                 solution = tf.argmax(logits, axis=2, output_type=tf.int32)
-            solution = task.map_to_integers(solution)
+            print('solution', solution)
             np.save(os.path.join(logging_dir, "solution.npy"), solution)
 
             # evaluate the solutions found by the model
